87e4d4035314b80ce8251c66b6a9933eab0783ac
Астахов Артём <artemastahov27@gmail.com>
2025-08-21 15:53:21 +0300

    first migration setup

---
+++ services/PassengerService/src/test/java/by/astakhau/trainee/passengerservice/PassengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.astakhau.trainee.passengerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
