5c8f5c7ba3aee23504f1bea7e84403a4263c3cb1
Астахов Артём <artemastahov27@gmail.com>
2025-08-21 23:28:17 +0300

    created drevires and cars scheme

---
+++ services/DriverService/src/test/java/by/astakhau/trainee/driverservice/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.astakhau.trainee.driverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
